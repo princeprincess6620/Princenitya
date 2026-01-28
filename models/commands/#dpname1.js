a74d56366db8af5f027b5dcfa6553f1575866503
princeprincess6620 <arshkhana880@gmail.com>
2026-01-28 12:36:10 +0530

    Update #dpname1.js

--- models/commands/#dpname1.js
+++ models/commands/#dpname1.js
@@ -75,7 +75,7 @@ const line1 = await module.exports.wrapText(ctx, text[0], 800);
 const line2 = await module.exports.wrapText(ctx, text[1], 733);
 
 ctx.fillText(line1.join("\n"), 105, 220);
-ctx.fillText(line2.join("\n"), 220, 105);
+ctx.fillText(line2.join("\n"), 40, 50);
 
 fs.writeFileSync(imgPath, canvas.toBuffer());
 
