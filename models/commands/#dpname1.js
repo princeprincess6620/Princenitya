cbe08f6e251e3f4c4f46270de81f3d2c424ef5ad
princeprincess6620 <arshkhana880@gmail.com>
2026-01-28 11:22:59 +0530

    Update #dpname1.js

--- models/commands/#dpname1.js
+++ models/commands/#dpname1.js
@@ -74,8 +74,8 @@ ctx.textAlign = "center";
 const line1 = await module.exports.wrapText(ctx, text[0], 800);
 const line2 = await module.exports.wrapText(ctx, text[1], 733);
 
-ctx.fillText(line1.join("\n"), 100, 300);
-ctx.fillText(line2.join("\n"), 255, 895);
+ctx.fillText(line1.join("\n"), 50, 230);
+ctx.fillText(line2.join("\n"), 600, 50);
 
 fs.writeFileSync(imgPath, canvas.toBuffer());
 
